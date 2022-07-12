adf97c509a651d42caa19ac4d46fa6c5fee933a6
Aliaksei_Baryliuk <Aliaksei_Baryliuk@epam.com>
2022-07-12 19:20:40 +0300

    tests

---
+++ src/main/java/com/epam/test/autocode/compare/UserLastNameComparator.java
@@ -0,0 +1,12 @@
+package com.epam.test.autocode.compare;
+
+import com.epam.test.autocode.entity.User;
+
+import java.util.Comparator;
+
+public class UserLastNameComparator implements Comparator<User> {
+    @Override
+    public int compare(User u1, User u2) {
+        return u1.getLastName().compareTo(u2.getLastName());
+    }
+}
--- src/test/java/com/epam/test/autocode/repository/UserRepositoryTest.java
+++ src/test/java/com/epam/test/autocode/repository/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.epam.test.autocode.repository;
 
 import com.epam.test.autocode.compare.UserFirstNameComparator;
+import com.epam.test.autocode.compare.UserLastNameComparator;
 import com.epam.test.autocode.entity.User;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -17,13 +18,14 @@ class UserRepositoryTest {
 
     private List<User> testUsers = new ArrayList<>();
     private UserRepository repository = new UserRepository();
-    private UserFirstNameComparator firstNameComparator = new UserFirstNameComparator();
-    private Integer initUserCount = 9;
-    private Comparator<User> userFNC = Comparator.comparing(User::getFirstName);
-    private Comparator<User> userLNC = Comparator.comparing(User::getLastName);
+    private final UserFirstNameComparator firstNameComparator = new UserFirstNameComparator();
+    private final UserLastNameComparator lastNameComparator = new UserLastNameComparator();
+    private final Comparator<User> userFNC = Comparator.comparing(User::getFirstName);
+    private final Comparator<User> userLNC = Comparator.comparing(User::getLastName);
 
     @BeforeAll
     public void init() {
+        Integer initUserCount = 9;
         testUsers  = repository.createUsers(initUserCount);
     }
     @Test
@@ -31,6 +33,24 @@ class UserRepositoryTest {
 
         List<User> afterComparing = testUsers.stream()
                 .sorted(firstNameComparator)
+                .collect(Collectors.toList());
+
+        assertFalse(isSorted(afterComparing, userFNC));
+    }
+    @Test
+    void givenUserLastNameComparator_whenUsersCompare_thenSuccess(){
+
+        List<User> afterComparing = testUsers.stream()
+                .sorted(firstNameComparator)
+                .collect(Collectors.toList());
+
+        assertTrue(isSorted(afterComparing, userFNC));
+    }
+    @Test
+    void givenUserLastNameComparator_whenUsersCompare_thenFalse(){
+
+        List<User> afterComparing = testUsers.stream()
+                .sorted(lastNameComparator)
                 .collect(Collectors.toList());
 
         assertFalse(isSorted(afterComparing, userFNC));
