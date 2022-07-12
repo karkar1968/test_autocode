726023237e66d616082dc2137ca3f8baf09d9362
Aliaksei_Baryliuk <Aliaksei_Baryliuk@epam.com>
2022-07-12 16:15:45 +0300

    test commit2

---
+++ src/main/java/com/epam/test/autocode/compare/UserFirstNameComparator.java
@@ -0,0 +1,12 @@
+package com.epam.test.autocode.compare;
+
+import com.epam.test.autocode.entity.User;
+
+import java.util.Comparator;
+
+public class UserFirstNameComparator implements Comparator<User> {
+    @Override
+    public int compare(User u1, User u2) {
+        return u1.getFirstName().compareTo(u2.getFirstName());
+    }
+}
